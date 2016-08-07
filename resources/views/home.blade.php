5a76e479336f9472a5f34d9cdbfc2b95e9806dc5
Guillaume Leclerc <guill.bout@gmail.com>
2016-08-07 12:44:09 +0800

    Remove navigation from home, added in a master layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,4 @@
 
 @section('content')
 
-    <ul>
-        <li><a href="/members">List of Members</a></li>
-        <li><a href="/meetings">List of Past Meetings</a></li>
-        <li><a href="/meetings/create">Coming Meeting Details</a></li>
-    </ul>
-
 @endsection
