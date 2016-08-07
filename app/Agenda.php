a3015f5459bc8b67ae83a3b42065e7cf8d75621f
Guillaume Leclerc <guill.bout@gmail.com>
2016-08-07 12:45:25 +0800

    Add Agenda model and controller

--- app/Agenda.php
+++ app/Agenda.php
@@ -6,5 +6,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class Agenda extends Model
 {
-    //
+    protected $dates = [
+        'date',
+    ];
+    
+    protected $fillable = [
+        'date',
+        'time',
+        'room',
+        'theme',
+        'opening',
+        'welcome',
+        'tme',
+        'speaker1',
+        'speaker2',
+        'speaker3',
+        'speaker4',
+        'tablemaster',
+        'evaluator1',
+        'evaluator2',
+        'evaluator3',
+        'evaluator4',
+        'timer',
+        'ahcounter',
+        'grammarian',
+        'general',
+        'closing',
+    ];
 }
