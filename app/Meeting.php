30ed99ed4a4ea0aa1e0c3b633f895eaf0ba91110
Guillaume Leclerc <guill.bout@gmail.com>
2016-08-07 12:45:52 +0800

    Add fillable fields for Meeting model so that saving doesn't crash

--- app/Meeting.php
+++ app/Meeting.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Meeting extends Model
 {
-    protected $guarded = ['id'];
+    protected $fillable = [
+        'date',
+        'time',
+        'room',
+        'theme',
+        'opening',
+        'welcome',
+        'tme',
+        'speaker1',
+        'speaker2',
+        'speaker3',
+        'speaker4',
+        'tablemaster',
+        'evaluator1',
+        'evaluator2',
+        'evaluator3',
+        'evaluator4',
+        'timer',
+        'ahcounter',
+        'grammarian',
+        'general',
+        'closing',
+        'coming',
+    ];
 }
