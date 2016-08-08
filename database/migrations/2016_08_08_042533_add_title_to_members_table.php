36a8231c1391e42871ff09a50c213f8f78f4141a
Guillaume Leclerc <guill.bout@gmail.com>
2016-08-08 13:13:56 +0800

    Add title to Member table and class

---
+++ database/migrations/2016_08_08_042533_add_title_to_members_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTitleToMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('members', function (Blueprint $table) {
+            $table->string('title');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('members', function (Blueprint $table) {
+            $table->dropColumn('title');
+        });
+    }
+}
