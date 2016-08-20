ad6c882aacb123d76476c3eeaee4b953bd3043da
Guillaume Leclerc <guill.bout@gmail.com>
2016-08-20 15:26:10 +0800

    Add migration for projects

---
+++ database/migrations/2016_07_30_084036_create_projects_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('category');
+            $table->integer('number');
+            $table->string('title');
+            $table->string('limit');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            Schema::drop('projects');
+        });
+    }
+}
